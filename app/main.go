aef4d4d160ebdcbae0d49a06b8aa6bba20c16ffc
yarins0 <yarinso39@gmail.com>
2026-05-18 13:16:08 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -55,6 +55,5 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
-	// TODO: Uncomment the line below to pass the first stage
-	// fmt.Print(resp.Choices[0].Message.Content)
+	fmt.Print(resp.Choices[0].Message.Content)
 }
